8b014c3d34aaa25afa0fc1000ca3a40a5e9d5257
NATALIASTH <45878269+NATALIASTH@users.noreply.github.com>
2021-02-08 17:47:08 +0100

    inicio de la grafica

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+plt.plot([17,18,19,20,25,30,35,40])
+plt.ylabel('Edad media equipo')
+plt.xlabel('Precio media equipo (en millones)')
+plt.show()
+
+#hacemos una gráfica para cada equipo y abajo sumamos el valor del equipo
+#y tambien cogemos las edades del equipo y hacemos la media
+#con la media de la edad y el valor de todos los jugadores mostramos la gráfica
